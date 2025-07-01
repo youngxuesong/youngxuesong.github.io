c70d4da6adbf190a3934b7a653262f3d5570749f
younger <younger@xiangyangchenghe.dpdns.org>
2025-07-01 23:29:31 +0800

    Site updated: 2025-07-01 23:29:28

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["posts/4d01862e/","posts/7517973c/","posts/be811694/","posts/1319af8/","posts/56e727f8/","posts/8690e815/","posts/4a17b156/","posts/560d8f6/","posts/560d7f1/","posts/560d8f5/","posts/80034969/","posts/ac56d399/"];function toRandomPost(){
+var posts=["posts/4d01862e/","posts/7517973c/","posts/be811694/","posts/1319af8/","posts/8690e815/","posts/56e727f8/","posts/4a17b156/","posts/560d7f1/","posts/560d8f6/","posts/560d8f5/","posts/80034969/","posts/ac56d399/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
